51b5a180d24706a516718e99ec0c39ee6860b431
Guilherme <360guilherme@gmai.com>
2020-03-11 12:00:40 -0300

    [UPDATE] adicionando calculo do valor a ser digitado, a partir do valor total que a transferencia deve custar

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <WesternUnion/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import WesternUnion from './components/WesternUnion';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    WesternUnion,
   },
 
   data: () => ({
--- src/components/WesternUnion.vue
+++ src/components/WesternUnion.vue
@@ -1,10 +1,49 @@
 <template>
+<v-card>
+    <v-card-title>
+        Western Union Simulator
+    </v-card-title>
+    <v-card-text>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                            v-model="valorTotal"
+                            label="Valor Total"
+                            outlined
 
+                    ></v-text-field>
+                </v-col>
+                <v-col>
+                    {{valorInicial}}
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card-text>
+</v-card>
 </template>
 
 <script>
     export default {
-        name: "WesternUnion"
+        name: "WesternUnion",
+        data: () => ({
+            valorTotal: 0,
+            IOF: 0.38,
+            taxaWU: 68.64,
+        }),
+
+        computed: {
+            valorInicial() {
+                const valor = this.porcentagemReversa(this.valorTotal - this.taxaWU, this.IOF);
+                return valor;
+            },
+        },
+
+        methods: {
+            porcentagemReversa(valorTotal, porcentagem){
+                return ((valorTotal) * 100 ) / (porcentagem + 100);
+            }
+        }
     }
 </script>
 
