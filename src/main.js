c6a402249c32cfb3d841b3d22403faef0dd6555b
Guilherme <360guilherme@gmai.com>
2020-03-11 18:29:23 -0300

    [ADD] adicionando font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import fontawesome from './plugins/fontawesome'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  fontawesome,
   render: h => h(App)
 }).$mount('#app')
